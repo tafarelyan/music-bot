d29beb6f79f6d2b2a615ae737ed66b84baf75f94
Tafarel Yan <ArrowsX@users.noreply.github.com>
2016-09-15 01:35:20 -0300

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
 from sqlalchemy import create_engine
 
-ENGINE = create_engine('mysql+pymysql://tafarel:y@ndata4@192.168.1.112/musicbot')
-TOKEN = '241673243:AAFeA51Rs5qtrf6bNr38IlWWOzo83QCQBvI'
+ENGINE = create_engine('check-sqlalchemy-for-more-info')
+TOKEN = 'YOUR-TOKEN'
